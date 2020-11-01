15dce3ad27772cd0421ff4c83b967f5e30a85d43
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-11-01 18:31:24 -0300

    Adiciona sentimentos

---
+++ SonyCodeStage/Source/Model/Mood.swift
@@ -0,0 +1,36 @@
+//
+//  Mood.swift
+//  SonyCodeStage
+//
+//  Created by Ailton Vieira Pinto Filho on 01/11/20.
+//
+
+import Foundation
+
+enum Mood: String {
+    case angry = "bravo"
+    case calm = "calmo"
+    case confused = "confuso"
+    case depressed = "deprimido"
+    case distressed = "angustiado"
+    case eager = "Ansioso"
+    case excited = "excitado"
+    case frustrated = "frustrado"
+    case furious = "furioso"
+    case glad = "contente"
+    case happy = "feliz"
+    case hungry = "faminto"
+    case loved = "apaixonado"
+    case optimistic = "otimista"
+    case positive = "positivo"
+    case relaxed = "relaxado"
+    case sad = "triste"
+    case sentimental = "sentimental"
+    case sick = "doente"
+    case surprised = "surpreso"
+    case thankful = "agradecido"
+    case tired = "cansado"
+    case touched = "emocionado"
+    case unhappy = "infeliz"
+    case worried = "preocupado"
+}
