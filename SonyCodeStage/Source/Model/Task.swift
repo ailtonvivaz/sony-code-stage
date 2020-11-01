f2ca3238ab096912aca0e8ba3dbd627b9b4b50a6
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-11-01 18:40:23 -0300

    Adiciona atividades

---
+++ SonyCodeStage/Source/Model/Task.swift
@@ -0,0 +1,20 @@
+//
+//  Task.swift
+//  SonyCodeStage
+//
+//  Created by Ailton Vieira Pinto Filho on 01/11/20.
+//
+
+import Foundation
+
+enum Task: String {
+    case work = "Trabalhando"
+    case study = "Estudando"
+    case meditation = "Meditando"
+    case workout = "Fazendo Exercícios"
+    case cleanHouse = "Limpando a casa"
+    case walk = "Caminhando"
+    case goWork = "Indo para o trabalho"
+    case goStudy = "Indo estudar"
+    case playGame = "Jogando"    
+}
